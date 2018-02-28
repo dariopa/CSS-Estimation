fa56ceaef5d4f7eec5b515695362d881054eea60
dariopa <dariopa@ethz.ch>
2018-02-28 13:16:22 +0100

    test

--- Models/Preprocessing.py
+++ Models/Preprocessing.py
@@ -14,7 +14,7 @@ import numpy as np
 call_folder = '/scratch_net/biwidl102/dariopa/Data_224_224'
 
 # How much data to use?
-use_data = 0.5
+use_data = 1
 
 # Divison factor for Training, Validation and Test data [0,1]:
 Train_split = 7./10
