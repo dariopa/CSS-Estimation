5b8fbe697b17a3fa6d0a93e77426e58e7b2a780d
dariopa <dariopa@ethz.ch>
2018-03-07 10:27:20 +0100

    test

--- Models/Pipeline_Generation.py
+++ Models/Pipeline_Generation.py
@@ -3,7 +3,7 @@ from Gen_Pre import Generate, Preprocess
 
 #################################
 # Generate images?
-generate = False
+generate = True
 # Preprocess data?
 preprocess = True
 #################################
