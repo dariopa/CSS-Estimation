8e4c0fc711f445dde59d1e90afcd3e0b85fd61ee
dariopa <dariopa@ethz.ch>
2018-03-10 20:34:42 +0100

    new training with histogram

---
+++ Models/Old_2018_03_10/Main.py
@@ -0,0 +1,111 @@
+import os
+import numpy as np
+# os.environ["CUDA_VISIBLE_DEVICES"] = os.environ['SGE_GPU']
+import tensorflow as tf
+from utils import train, predict, save, load
+from PIL import Image
+from Neural_Networks import NeuralNetworks
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True #Do not assign whole gpu memory, just use it on the go
+config.allow_soft_placement = True #If an operation is not define it the default device, let it execute in another.
+
+
+##############################################################################
+# Folder Path
+# call_folder = '/scratch_net/biwidl102/dariopa/Data_32_32/'
+call_folder = '/scratch_net/biwidl102/dariopa/Data_150_150/'
+# call_folder = '/scratch_net/biwidl102/dariopa/Data_150_150_5_classes/'
+# call_folder = '/scratch_net/biwidl102/dariopa/Data_224_224/'
+# call_folder = '/scratch_net/biwidl102/dariopa/Data_224_224_5_classes/'
+
+# call_folder = '/home/dario/Documents/SemThes_Local/Data_32_32/'
+# call_folder = '/home/dario/Documents/SemThes_Local/Data_150_150/'
+# call_folder = '/home/dario/Documents/SemThes_Local/Data_224_224/'
+
+store_folder = './model_r_alpha_10_classes_VGG16_150/' 
+if not os.path.isdir(store_folder):
+    os.makedirs(store_folder)
+Name = 'r_alpha'
+
+## Define hyperparameters
+learning_rate = 2e-4
+random_seed = 123
+np.random.seed(random_seed)
+batch_size = 64
+epochs = 150
+
+# Select Net
+# CNN = NeuralNetworks.build_LeNet_own
+CNN = NeuralNetworks.build_VGG16
+
+# Classes
+classes = 10
+
+##############################################################################
+# IMPORT DATA
+
+X_train = np.load(call_folder + 'X_train.npy')
+X_valid = np.load(call_folder + 'X_validation.npy')
+X_test = np.load(call_folder + 'X_test.npy')
+y_train = np.load(call_folder + 'y_binned_train.npy')[:, 0]
+y_valid = np.load(call_folder + 'y_binned_validation.npy')[:, 0]
+y_test = np.load(call_folder + 'y_binned_test.npy')[:, 0]
+
+img = np.asarray(Image.open(X_train[0]), dtype=np.uint8)
+print(img.shape)
+x_row, y_col,_ = img.shape
+del img
+
+print('Training:   ', X_train.shape, y_train.shape)
+print('Validation: ', X_valid.shape, y_valid.shape)
+print('Test Set:   ', X_test.shape, y_test.shape)
+
+##############################################################################
+# GRAPH TRAINING
+
+## create a graph
+g = tf.Graph()
+with g.as_default():
+    tf.set_random_seed(random_seed)
+    ## build the graph
+    CNN(classes, x_row, y_col, learning_rate)
+
+##############################################################################
+# TRAINING
+print()
+print('Training... ')
+with tf.Session(graph=g, config=config) as sess:
+    [avg_loss_plot, val_accuracy_plot] = train(sess, epochs=epochs,
+                                               training_set=(X_train, y_train),
+                                               validation_set=(X_valid, y_valid),
+                                               batch_size=batch_size,
+                                               initialize=True)
+    np.save(os.path.join(store_folder, Name + '_avg_loss_plot.npy'), avg_loss_plot)
+    np.save(os.path.join(store_folder, Name + '_val_accuracy_plot.npy'), val_accuracy_plot)
+##############################################################################
+# PREDICTION
+
+    # LABELS
+    y_pred = np.full((len(X_test)),0)
+    X = np.full((1, x_row, y_col, 1), 0)                              # NUMERISCHER WERT - ÄNDERN!
+
+    for i in range(len(X_test)):
+        X[0, :, :, :] = np.array(Image.open(str(X_test[i])))[:,:,0:1] # NUMERISCHER WERT - ÄNDERN!
+        y_pred[i] = predict(sess, X, return_proba=False)              # NUMERISCHER WERT - ÄNDERN!
+    test_acc = 100*np.sum((y_pred == y_test)/len(y_test))
+    print(' Test Acc: %7.3f%%' % test_acc)
+    with open(os.path.join(store_folder, Name + '_AccuracyTest.txt'), 'w') as f:
+        f.write('%.3f%%' % (test_acc))
+
+    # PROBABILITIES
+    np.set_printoptions(precision=3, suppress=True)
+
+    y_pred_proba = np.full((len(X_test), classes), 0, dtype=np.float16)     # NUMERISCHER WERT - ÄNDERN!
+    X = np.full((1, x_row, y_col, 1), 0, dtype=np.uint8)
+
+    for i in range(len(X_test)):
+        X[0, :, :, :] = np.array(Image.open(str(X_test[i])))[:, :, 0:1] # NUMERISCHER WERT - ÄNDERN!
+        y_pred_proba[i] = predict(sess, X, return_proba=True)
+    print(y_pred_proba)
+    np.save(os.path.join(store_folder, Name + '_pred_proba.npy'), y_pred_proba)
---
+++ Models/Old_2018_03_10/utils.py
@@ -0,0 +1,99 @@
+import os
+import numpy as np
+os.environ["CUDA_VISIBLE_DEVICES"] = os.environ['SGE_GPU']
+import tensorflow as tf
+from sklearn.utils import shuffle
+from PIL import Image
+import time
+
+
+def save(saver, sess, epoch, path):
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    print('Saving model in %s' % path)
+    saver.save(sess, os.path.join(path, 'cnn-model.ckpt'), global_step=epoch)
+
+
+def load(saver, sess, path, epoch):
+    print('Loading model from %s' % path)
+    saver.restore(sess, os.path.join(path, 'cnn-model.ckpt-%d' % epoch))
+
+
+def batch_generator(X_train, y_train, batch, i, row, col, loops):
+    
+    if i == (loops-1):
+        batch_size = len(X_train) - (i * batch)
+    else:
+        batch_size = batch
+
+    X_send = np.full((batch_size, row, col, 1), 0, dtype = np.uint8)
+    
+    for k in range(0, batch_size):
+        img = np.array(Image.open(str(X_train[k + i * batch])))
+        X_send[k, :, :, :] = img[:,:,0:1]                         # NUMERISCHER WERT - ÄNDERN!
+    y_send = y_train[i * batch:(i + 1) * batch]
+    # print(len(X_send), len(y_send))
+
+    (X_send, y_send) = shuffle(X_send, y_send)
+
+    return(X_send, y_send)
+
+def train(sess, epochs, training_set, validation_set,
+          batch_size, initialize=True, dropout=0.5):
+
+    X_data_train = np.array(training_set[0])
+    y_data_train = np.array(training_set[1])
+    training_loss = []
+
+    # initialize variables
+    if initialize:
+        sess.run(tf.global_variables_initializer())
+
+    avg_loss_plot = []
+    val_accuracy_plot = []
+    row, col, _ = np.array(Image.open(str(X_data_train[0]))).shape
+    
+    loops = int(np.floor(len(X_data_train) / batch_size)) + 1
+    print('Loops: ', loops)
+    for epoch in range(1, epochs+1):
+        avg_loss = 0.0   
+        start_time = time.time()
+        for i in range(0, loops):
+            batch_x, batch_y = batch_generator(X_data_train, y_data_train, batch=batch_size, i=i, row=row, col=col, loops=loops)
+            feed = {'tf_x:0': batch_x, 'tf_y:0': batch_y, 'fc_keep_prob:0': dropout}
+            loss, _ = sess.run(['cross_entropy_loss:0', 'train_op'], feed_dict=feed)
+            avg_loss += loss
+
+        end_time = time.time()
+        print("\nTotal time taken this loop [s]: ", end_time - start_time)
+        if epoch == 1:
+            print('Termination time will be:  ', time.ctime(start_time + (end_time - start_time)*epochs))
+
+        avg_loss_plot.append(np.mean(avg_loss))
+        print('Epoch %02d Training Avg. Loss: %7.4f' % (epoch, avg_loss), end=' ')
+        del batch_x, batch_y
+
+        if validation_set is not None:
+            X_data = np.array(validation_set[0])
+            y_data = np.array(validation_set[1])
+            y_pred = np.full((len(X_data)), 0)
+            x_row, y_col, _ = np.array(Image.open(str(X_data[0]))).shape
+            X = np.full((1, x_row, y_col, 1), 0)
+
+            for i in range(len(X_data)):
+                X[0, :, :, :] = np.array(Image.open(str(X_data[i])))[:, :, 0:1] # NUMERISCHER WERT - ÄNDERN!
+                y_pred[i] = predict(sess, X, return_proba=False)
+            valid_acc = 100*np.sum((y_pred == y_data)/len(y_data))
+            val_accuracy_plot.append(valid_acc)
+            print(' Validation Acc: %7.3f%%' % valid_acc)
+        else:
+            print()
+
+    return avg_loss_plot, val_accuracy_plot
+
+def predict(sess, X, return_proba=False):
+    feed = {'tf_x:0': X, 'fc_keep_prob:0': 1.0}
+    if return_proba:
+        return sess.run('probabilities:0', feed_dict=feed)
+    else:
+        return sess.run('labels:0', feed_dict=feed)
