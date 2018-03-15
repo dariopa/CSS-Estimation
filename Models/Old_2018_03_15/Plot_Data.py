002cca5502ea3302aa64dbea85ddb53ce577cf30
Dario Panzuto <dariopa@users.noreply.github.com>
2018-03-15 22:35:35 +0100

    new part of code, after Radu's inputs
    -

---
+++ Models/Old_2018_03_15/Plot_Data.py
@@ -0,0 +1,34 @@
+import numpy as np
+from matplotlib.pyplot import imshow
+import matplotlib.pyplot as plt
+
+##############################################################################
+Name = 'r_alpha'
+# call_folder = '/home/dario/Desktop/SemThes/Models/model_r_alpha_3_classes_LeNet_150/'
+# call_folder = '/home/dario/Desktop/SemThes/Models/model_r_alpha_3_classes_VGG16_150/'
+# call_folder = '/home/dario/Desktop/SemThes/Models/model_r_alpha_10_classes_LeNet_150/'
+# call_folder = '/home/dario/Desktop/SemThes/Models/model_r_alpha_10_classes_VGG16_224/'
+# call_folder = '/home/dario/Desktop/SemThes/Models/model_r_alpha_5_classes_VGG16_150/'
+call_folder = '/home/dario/Desktop/SemThes/Models/model_r_alpha_10_classes_VGG16_150/'
+# call_folder = '/home/dario/Desktop/SemThes/Models/model_r_alpha_5_classes_VGG16_224/'
+##############################################################################
+
+avg_loss_plot = np.load(call_folder + Name + '_avg_loss_plot.npy')
+val_accuracy_plot = np.load(call_folder + Name + '_val_accuracy_plot.npy')
+# test_accuracy_plot = np.load(call_folder + Name + '_test_accuracy_plot.npy')
+
+plt.figure(1)
+plt.plot(range(1, len(avg_loss_plot) + 1), avg_loss_plot)
+plt.title('Training loss for ' + Name)
+plt.xlabel('Epoch')
+plt.ylabel('Average Training Loss')
+plt.savefig(call_folder + Name + '_TrainLoss.jpg')
+
+plt.figure(2)
+plt.plot(range(1, len(val_accuracy_plot) + 1), val_accuracy_plot, label='Validation Accuracy')
+# plt.plot(range(1, len(test_accuracy_plot) + 1), test_accuracy_plot, label='Test Accuracy')
+plt.title('Accuracy for ' + Name)
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.savefig(call_folder + Name + '_Acccuracy.jpg')
---
+++ Models/Old_2018_03_15/README.txt
@@ -0,0 +1,7 @@
+First results, with 224x224 and 150x150 image crops. Tried LeNet and VGG16:
+LeNet proofs to have shown worse performance. 
+- Tried with many epochs - but now realised that not more than 40 are needed. 
+- only 1 channel analysed.
+- no preprocessing of image. 
+- only classification, no regression. 
+
