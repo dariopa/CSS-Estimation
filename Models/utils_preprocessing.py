fb605ba26c068b59a2062092b97b4a62c6cddc99
Dario Panzuto <dariopa@users.noreply.github.com>
2018-03-16 23:16:22 +0100

    introduced standardization as preprocessing
    -

--- Models/utils_preprocessing.py
+++ Models/utils_preprocessing.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def standardize(X_data):
-    # X_new = (X_data - np.mean(X_data)) / np.std(X_data)
     X_new = X_data
+    X_data = (X_data[0, :, :, 0] - np.mean(X_data[0, :, :, 0])) / np.std(X_data[0, :, :, 0])
+    X_new[0:1, :, :, 0:1] = X_data
     return X_new
