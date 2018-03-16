edf56e42fd89ddd64cf4b07db54ea20499bde218
dariopa <dariopa@ethz.ch>
2018-03-16 16:30:02 +0100

    fix0

--- Models/utils_preprocessing.py
+++ Models/utils_preprocessing.py
@@ -1,5 +1,6 @@
 import numpy as np
 
 def standardize(X_data):
+    X_new = X_data
     X_new[:, :, 0:1] = (X_data[:, :, 0:1] - np.mean(X_data[:, :, 0:1])) / np.std(X_data[:, :, 0:1])
     return X_new
