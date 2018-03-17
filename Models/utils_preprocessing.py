940adeffb9a6a70c4365d060aba7cfdd0b179b75
dariopa <dariopa@ethz.ch>
2018-03-18 00:41:26 +0100

    try unpreprocessed data without gradientdescenoptimizer

--- Models/utils_preprocessing.py
+++ Models/utils_preprocessing.py
@@ -1,5 +1,5 @@
 import numpy as np
 
 def standardize(X_data):
-    X_data = (X_data - np.mean(X_data)) / np.std(X_data)
+    # X_data = (X_data - np.mean(X_data)) / np.std(X_data)
     return X_data
