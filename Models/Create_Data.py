b69b32b12d6f795125c9d9fac4837f4a38b0daab
dariopa <dariopa@ethz.ch>
2018-06-11 13:31:13 +0200

    fixed crate-data- code

--- Models/Create_Data.py
+++ Models/Create_Data.py
@@ -9,45 +9,6 @@ import cv2
 from PIL import Image
 from sklearn.utils import shuffle
 
-# Generate Data?
-generate = True
-# Split Data?
-split = True
-
-call_folder = '../Images_RAD/'
-store_folder = '../Data_Ambiguity'
-if not os.path.isdir(store_folder):
-    os.makedirs(store_folder)
-if not os.path.isdir(os.path.join(store_folder, 'Images')):
-    os.makedirs(os.path.join(store_folder, 'Images'))
-
-# FOR IMAGE GENERATION ----------------------
-# Batch size of images
-X_shape = 1000
-Y_shape = 1000
-
-alpha = np.array([[0.500, 0.533, 0.566, 0.600, 0.500, 0.533, 0.566, 0.600, 0.500, 0.533, 0.566, 0.600, 0.500, 0.533, 0.566, 0.600]])
-r_mean = 615
-g_mean = 530
-b_mean = 465
-sigma = np.array([[28., 30., 32., 34., 34., 28., 30., 32., 32., 34., 28., 30., 30., 32., 34., 28.]])
-
-# classes
-classes = len(alpha)
-
-# How much data to use?
-use_data = 1.
-
-# Divison factor for Training, Validation and Test data [0,1]:
-Train_split = 8./10
-Val_split = 1./10
-Test_split = 1./10
-
-if generate == True:
-    print('Generating images')
-    Generate(call_folder, store_folder, X_shape, Y_shape, alpha, r_mean, g_mean, b_mean, sigma, classes)
-
-
 def Generate(call_folder, store_folder, X_shape, Y_shape, alpha, r_mean, g_mean, b_mean, sigma, classes):
     _, nr_param = alpha.shape
     nr_hyp_images = len(fnmatch.filter(os.listdir(call_folder), '*.mat'))
@@ -170,3 +131,42 @@ def Generate(call_folder, store_folder, X_shape, Y_shape, alpha, r_mean, g_mean,
     np.save(store_folder + '/datapath.npy', data)
 
     print('Done!')
+
+# Generate Data?
+generate = True
+# Split Data?
+split = True
+
+call_folder = '../Images_RAD/'
+store_folder = '../Data_Ambiguity'
+if not os.path.isdir(store_folder):
+    os.makedirs(store_folder)
+if not os.path.isdir(os.path.join(store_folder, 'Images')):
+    os.makedirs(os.path.join(store_folder, 'Images'))
+
+# FOR IMAGE GENERATION ----------------------
+# Batch size of images
+X_shape = 1000
+Y_shape = 1000
+
+alpha = np.array([[0.500, 0.533, 0.566, 0.600, 0.500, 0.533, 0.566, 0.600, 0.500, 0.533, 0.566, 0.600, 0.500, 0.533, 0.566, 0.600]])
+r_mean = 615
+g_mean = 530
+b_mean = 465
+sigma = np.array([[28., 30., 32., 34., 34., 28., 30., 32., 32., 34., 28., 30., 30., 32., 34., 28.]])
+
+# classes
+classes = len(alpha)
+
+# How much data to use?
+use_data = 1.
+
+# Divison factor for Training, Validation and Test data [0,1]:
+Train_split = 8./10
+Val_split = 1./10
+Test_split = 1./10
+
+if generate == True:
+    print('Generating images')
+    Generate(call_folder, store_folder, X_shape, Y_shape, alpha, r_mean, g_mean, b_mean, sigma, classes)
+
